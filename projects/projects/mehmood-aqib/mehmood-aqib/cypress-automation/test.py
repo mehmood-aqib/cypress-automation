8e51d89d71ed4f301515e6645088dfc4e99b1807
akira <akira@drox.com>
2024-11-18 13:37:02 +0000

    add fibonacci function to test.py

---
+++ projects/projects/mehmood-aqib/mehmood-aqib/cypress-automation/test.py
@@ -0,0 +1,8 @@
+def fibonacci_series(n):
+    fib_series = [0, 1]
+    while len(fib_series) < n:
+        fib_series.append(fib_series[-1] + fib_series[-2])
+    return fib_series
+
+# Write Fibonacci series up to 10
+print(fibonacci_series(10))
